688907f891c2f103612fc9fdb81e820607e09cc5
Noah Parker <noahtmparker@gmail.com>
2022-11-03 23:17:39 -0400

    big fix

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// Create a new instance of an Apollo server with the GraphQL schema
+// Create a new Instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate our Apollo server with the Express application as middleware
