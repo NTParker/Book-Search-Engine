73cf80c9198c65738f0147cb672118a25c4f2f39
Noah Parker <noahtmparker@gmail.com>
2022-11-03 22:53:31 -0400

    added build

---
+++ client/build/precache-manifest.e939e547674c5708e87e4cb4136bb5c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e53f2f1e514642e4e0effb3521df65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bd6875c498d3bac00ba",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "75db1842a6d3bfc770b7",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "2bd6875c498d3bac00ba",
+    "url": "/static/js/2.18f6e6c5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.18f6e6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75db1842a6d3bfc770b7",
+    "url": "/static/js/main.33afbf08.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
